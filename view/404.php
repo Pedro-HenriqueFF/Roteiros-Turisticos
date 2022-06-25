852159f433f31dbc937e3ec66694c923e0740e22
lucaslemo <lucaslemo852@gmail.com>
2022-06-25 13:32:37 -0300

    Página 404

---
+++ view/404.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Futurismo</title>
+        <link href="<?php echo BASE_URL; ?>/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div id="layoutError">
+            <div id="layoutError_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-6">
+                                <div class="text-center mt-4">
+                                    <img class="mb-4 img-error" src="<?php echo BASE_URL; ?>/assets/img/logos/error-404.svg"/>
+                                    <p class="lead">A URL buscada não foi encontrada em nosso servidor.</p>
+                                    <a href="<?php echo BASE_URL; ?>">
+                                        <i class="fas fa-arrow-left me-1"></i>
+                                        Voltar
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <footer class="bg-light fixed-bottom">
+            <div class="container px-0 px-lg-0">
+                <div class="row gx-4 gx-lg-5">    
+                    <div class="col-lg-4 ms-0 p-0 text-center align-self-center">
+                        <div class=""><img class="" src="<?php echo BASE_URL; ?>/assets/img/logos/ufca-logo.png" alt="ufca" width="100%" height="100%"></div>
+                    </div>
+                    <div class="col-lg-4 text-center align-self-center">
+                        <div class="small text-center text-muted">Copyright &copy; 2022 - Titan Tech</div>
+                    </div>
+                    <div class="col-lg-4 text-center  align-self-center">
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- JQuery-->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?php echo BASE_URL; ?>/js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
+
+            

f492c1d93ab5bcbda8f3dee7c43f007bb770475b
lucaslemo <lucaslemo852@gmail.com>
2022-06-27 07:58:49 -0300

    Locais Update

--- _bd/insert_locais.sql
+++ _bd/insert_locais.sql
@@ -21,3 +21,14 @@ INSERT INTO locais (nome_local, endereco_local, get_id_cidade, get_id_genero_1, 
   'Terça-Feira a Domingo das 08:00h as 15:00h',
   'A cadeira de balanço de palhinha logo na entrada do Memorial Patativa do Assaré remete muito aos últimos anos de vida do poeta. Era sentado nela, na sala de casa, que ele recebia as visitas e declamava seus versos. O espaço foi inaugurado em 1999, três anos antes de o homenageado falecer, aos 93. Abriga objetos de uso pessoal, a exemplo dos óculos escuros e o chapéu, além de manuscritos, títulos e troféus. Traduzidos em vários idiomas, seus livros foram estudados na cadeira de Literatura Popular Universal na Universidade de Sorbonne (França). Com vida simples de homem do campo e sem estudos, o poeta é um dos principais representantes da arte popular do século XX no Nordeste.',
   'assets/img/locais/Memorial Patativa do Assare/Memorial Patativa do Assare');
+
+  -- Museu de Paleontologia Plácido Cidade Nuvens
+INSERT INTO locais (nome_local, endereco_local, get_id_cidade, get_id_genero_1, get_id_genero_2, get_id_genero_3, valor_local, horario_local, descricao_local, pathing_local) VALUES
+  ('Museu de Paleontologia Plácido Cidade Nuvens',
+  'R.Plácido Cidade Nuvens, 326 - Santana do Cariri, CE, 63190-000',
+  10,
+  11, 4, null,
+  'Acesso gratuito',
+  'Terça-Feira a Domingo das 08:00h as 15:00h',
+  'O Museu de Paleontologia Plácido Cidade Nuvens é um museu brasileiro que abriga acervo paleontológico oriundo da região do Cariri e pertencente a Universidade Regional do Cariri.',
+  'assets/img/locais/Museu de Paleontologia Placido Cidade Nuvens/Museu de Paleontologia Placido Cidade Nuvens');
